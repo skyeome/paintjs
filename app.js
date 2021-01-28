f20c72e744fe868a69b9885cc396becf0f7d54b4
김성겸 <skyome@skyomeui-MacBookPro.local>
2021-01-29 00:14:58 +0900

    complete paintjs

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 const canvas = document.getElementById("jsCanvas");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 const ctx = canvas.getContext("2d");
+
+const INITIAL_COLOR = "#2c2c2c";
 
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
 
-
-
-ctx.strokeStyle = "#2c2c2c";
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,canvas.width,canvas.height);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
 }
 
 function startPainting(){
-    painting = true;
+  painting = true;
 }
 
 
@@ -33,14 +41,66 @@ function onMouseMove(event) {
   }
 }
 
-function onMouseDown(event) {
-  painting = true;
+function handleColorClick(event){
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
+function handleInputChange(event){
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick(){
+  if(filling === true){
+    filling = false;
+    mode.innerText = "Fill";
+  } else{
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
+function handleCanvasClick(){
+  if(filling){
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+  }
+  
+}
+
+function handleCM(event){
+  event.preventDefault();
+}
+
+function handleSaveClick(){
+  const image = canvas.toDataURL("image/png");
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS[🎨]";
+  link.click();
+}
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click",handleCanvasClick);
+  canvas.addEventListener("contextmenu",handleCM);
+}
+
+//Array.from(colors) object를 array로 변환하는 함수
+Array.from(colors).forEach(color=>color.addEventListener("click",handleColorClick));
+
+if(range){
+  range.addEventListener("input",handleInputChange);
+}
+
+if(mode){
+  mode.addEventListener("click",handleModeClick);
+}
+
+if(saveBtn){
+  saveBtn.addEventListener("click",handleSaveClick);
 }
